24f76eea557d3e783f71c2a86305091934f836a5
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2022-11-29 03:04:11 +0300

    Category filter and search

--- shop/views.py
+++ shop/views.py
@@ -3,19 +3,36 @@ from shop.models import Product
 from shop.models import category
 import json
 from django.http import JsonResponse
+from django.core.paginator import Paginator
 # Create your views here.
 def search(request):
+    print(request)
     if request.method == 'POST':
         search_text = json.loads(request.body).get('searchText')
+        categ = json.loads(request.body).get('category')
+        # print(search_text)
     else:
         search_text = ''
-    products = Product.objects.filter(name__icontains=search_text) | Product.objects.filter(description__icontains=search_text)
+    if categ:
+        print(categ)
+        products = Product.objects.filter(name__icontains=search_text, category=categ) | Product.objects.filter(description__icontains=search_text, category=categ)
+    else:
+        products = Product.objects.filter(name__icontains=search_text) | Product.objects.filter(description__icontains=search_text)
     data = products.values()
+    for product in data:
+        product['category'] = category.objects.get(id=product['category_id']).name
     # return render(request, 'ajax_search.html', {'products': products})
     return JsonResponse(list(data), safe=False)
-
 def home(request):
     products = Product.objects.all()
-    # get categories without parents
+    paginator = Paginator(products, 20)
     categories = category.objects.filter(parent=None)
-    return render(request, 'home.html', {'products': products, 'categories': categories})
+    page = paginator.get_page(request.GET.get('page'))
+    context = {
+        'page': page,
+        'products': products,
+        'categories': categories
+        }
+
+    # get categories without parents
+    return render(request, 'home.html', context)
