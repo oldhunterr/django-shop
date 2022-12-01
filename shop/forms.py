9dca40c7ab4ebc9f639b8ed279dbf0e1cf67ad8e
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2022-12-01 10:46:46 +0300

    after we bring eid

--- shop/forms.py
+++ shop/forms.py
@@ -10,9 +10,15 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'description', 'price', 'category', 'image',)
-
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control','style': 'height: 100px'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control' }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control' }),
+        }
 class ExtraImagesForm(forms.ModelForm):
-    img = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': 'True'}))
+    img = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': 'True', 'class': 'form-control'}))
     class Meta:
         model = extra_images
         fields = ('img',)
