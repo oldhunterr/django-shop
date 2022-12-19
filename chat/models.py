25cdc3a513f00f194e20ed33dbe9626c67584b9d
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2022-12-19 20:56:24 +0300

    slug models, validation

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from shop.models import Product
+from django.template.defaultfilters import slugify
 
 User = get_user_model()
 
@@ -7,9 +9,36 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey('Room', related_name='room_messages', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.author.username
-
+        return self.author.name
+    # get the last 10 messages
     def last_10_messages():
         return Message.objects.order_by('-timestamp').all()[:10]
+
+class Room(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, null=True)
+    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE)
+    member1 = models.ForeignKey(User, related_name='member1', on_delete=models.CASCADE)
+    member2 = models.ForeignKey(User, related_name='member2', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    # check if the user is in the room
+    def check_user(self, user):
+        if user == self.member1 or user == self.member2:
+            return True
+        return False
+    # get the second user in the room
+    def get_second_user(self, user):
+        if user == self.member1:
+            return self.member2
+        return self.member1
+    # save to change the slug
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify("chat-"+self.member1.name +"-"+ self.member2.name+"-"+self.product.name)
+        super(Room, self).save(*args, **kwargs)
+    
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from .models import Message, Room
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 import json
 
 def index(request):
@@ -8,6 +11,14 @@ def index(request):
 
 @login_required
 def room(request, room_name):
+    # check room if exist
+    if not Room.objects.filter(slug=room_name).exists():
+        # if not exist, raise 404
+        raise Http404("Room does not exist")
+    # check if user is in the room
+    room = Room.objects.get(slug=room_name)
+    if not room.check_user(request.user):
+        raise PermissionDenied("You are not in the room")
     return render(request, 'chat/room.html', {
         'avatar': request.user.avatar,
         'room_name_json': mark_safe(json.dumps(room_name)),
